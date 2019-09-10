ff56e830d25a79f87db03b36fc5b7e7dac764e22
cspiewak <33766126+cspiewak@users.noreply.github.com>
2019-09-10 09:25:01 -0700

    Update main.py

--- main.py
+++ main.py
@@ -2,16 +2,27 @@
 # Made on: 09-SEP-2019
 # Make a 8-bit greyscale 1081×1081 pixel image (PNG) to be used as a heightmap in City Skylines
 
-from os.path import dirname, abspath
+import random
 from PIL import Image, ImageDraw
 
-def drawFirst():
-    ImageDraw.ImageDraw.rectangle()
+def drawFirst(draw):
+    # Draw the lage rectangles/squares of no more than 4km by 4km in size (Max: 420px lol Min: 210px)
+    # Randomly generate the sizes
+    i = 1
+    while i <= 6:
+        x0 = random.randint(16, 645)
+        y0 = random.randint(16, 645)
+        startCord = (x0, y0)
+        x1 = x0 + random.randint(210, 420) # X length
+        y1 = y0 + random.randint(210, 420) # Y length
+        endCord = (x1, y1)
+        draw.rectangle((startCord, endCord), fill='hsl(240, 0%, 25%)') # (black=0%, normal=50%, white=100%)
+        i += 1
 
 def main():
-    PATH_NAME = dirname(dirname(abspath(__file__)))
-
-    img = Image.new('L', (1081, 1081)) # About 60 pixels per km of map
+    img = Image.new('RGB', (1081, 1081)) # About 60 pixels per km of map
+    draw = ImageDraw.Draw(img)
+    drawFirst(draw)
     img.save('generated_map.png') # Every itteration overwrites the last map
 
 if __name__ == "__main__":

e383072f9444a79f6e07723faf4306a74994de94
cspiewak <33766126+cspiewak@users.noreply.github.com>
2019-09-10 21:09:18 -0700

    Update main.py
    Changed xy values to move closer to build area

--- main.py
+++ main.py
@@ -10,13 +10,13 @@ def drawFirst(draw):
     # Randomly generate the sizes
     i = 1
     while i <= 6:
-        x0 = random.randint(16, 645)
-        y0 = random.randint(16, 645)
+        x0 = random.randint(100, 981)
+        y0 = random.randint(100, 981)
         startCord = (x0, y0)
-        x1 = x0 + random.randint(210, 420) # X length
-        y1 = y0 + random.randint(210, 420) # Y length
+        x1 = x0 + random.randint(210, 400) # X length
+        y1 = y0 + random.randint(210, 400) # Y length
         endCord = (x1, y1)
-        draw.rectangle((startCord, endCord), fill='hsl(240, 0%, 25%)') # (black=0%, normal=50%, white=100%)
+        draw.rectangle((startCord, endCord), fill='hsl(240, 0%, 6.25%)') # (black=0%, normal=50%, white=100%)
         i += 1
 
 def main():

356fefcb49343d03d2ce0a1c78a30c4645bb1620
cspiewak <33766126+cspiewak@users.noreply.github.com>
2019-09-11 10:13:11 -0700

    Update main.py
    Updated bounding box values. Added comments. Changed the shapes drawn from rectangles/squares to ellipses.

--- main.py
+++ main.py
@@ -6,21 +6,21 @@ import random
 from PIL import Image, ImageDraw
 
 def drawFirst(draw):
-    # Draw the lage rectangles/squares of no more than 4km by 4km in size (Max: 420px lol Min: 210px)
     # Randomly generate the sizes
+    n = random.randint(6, 12)
     i = 1
-    while i <= 6:
-        x0 = random.randint(100, 981)
-        y0 = random.randint(100, 981)
+    while i <= n:
+        x0 = random.randint(100, 600) # Starting point in the X direction defining the bounding box
+        y0 = random.randint(100, 600) # Starting point in the Y direction defining the bounding box
         startCord = (x0, y0)
-        x1 = x0 + random.randint(210, 400) # X length
-        y1 = y0 + random.randint(210, 400) # Y length
+        x1 = x0 + random.randint(275, 375) # X length
+        y1 = y0 + random.randint(275, 375) # Y length
         endCord = (x1, y1)
-        draw.rectangle((startCord, endCord), fill='hsl(240, 0%, 6.25%)') # (black=0%, normal=50%, white=100%)
+        draw.ellipse((startCord, endCord), fill='hsl(240, 0%, 6.25%)') # (black=0%, normal=50%, white=100%)
         i += 1
 
 def main():
-    img = Image.new('RGB', (1081, 1081)) # About 60 pixels per km of map
+    img = Image.new('RGB', (1081, 1081)) # About 16 pixels per km of map
     draw = ImageDraw.Draw(img)
     drawFirst(draw)
     img.save('generated_map.png') # Every itteration overwrites the last map

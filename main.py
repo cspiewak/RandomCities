c6d02abc314ca82d5dc3cc3452c4358ffbea67a0
cspiewak <33766126+cspiewak@users.noreply.github.com>
2019-09-11 16:04:43 -0700

    Update main.py
    Added placeholder for the second and third passes of drawing. As well as a placeholder for the test for the correct area to draw. Removed the need for the colorsys lib as the script was updated to use RGB instead of HSL.

--- main.py
+++ main.py
@@ -3,8 +3,13 @@
 # Make a 8-bit greyscale 1081×1081 pixel image (PNG) to be used as a heightmap in City Skylines
 
 import random
-import colorsys # Used to find the area of the previous obj generation
 from PIL import Image, ImageDraw
+
+def is_grey(img, testX, testY, i):
+    rgbVal = img.getpixel((testX, testY))
+    # make a case switch for all three drawings indicated by the i value
+    # use something better than i
+    # in each case test the color of pixel to see if it is a suitable starting area
 
 def drawFirst(draw):
     # Randomly generate the sizes
@@ -17,8 +22,14 @@ def drawFirst(draw):
         x1 = x0 + random.randint(275, 375) # X length
         y1 = y0 + random.randint(275, 375) # Y length
         endCord = (x1, y1)
-        draw.ellipse((startCord, endCord), fill='hsl(240, 0%, 6.25%)') # (black=0%, normal=50%, white=100%)
+        draw.ellipse((startCord, endCord), fill='rgb(16, 16, 16)')
         i += 1
+
+def drawSecond(draw):
+    # rgb(32, 32, 32)
+
+def drawThird(draw):
+    # rgb(64, 64, 64)
 
 def main():
     img = Image.new('RGB', (1081, 1081)) # About 16 pixels per km of map

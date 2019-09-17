d94f46718bc2abc670c6b81d0db6b3d101cd21d4
cspiewak <33766126+cspiewak@users.noreply.github.com>
2019-09-17 15:57:22 -0700

    Update main.py
    Added Filtering to smooth the topology. Time to start looking at ways to generate more natural topology.

--- main.py
+++ main.py
@@ -3,34 +3,20 @@
 # Make a 8-bit greyscale 1081×1081 pixel image (PNG) to be used as a heightmap in City Skylines
 
 import random
-import numpy as np
-from PIL import Image, ImageDraw
-
-"""def is_grey(val):
-    # In each case test the color of pixel to see if it is a suitable starting area
-    imgTest = Image.open('generated_map.png', 'r')
-    width, height = imgTest.size
-    pixel_values = list(imgTest.getdata())
-    pixel_values = np.array(pixel_values).reshape((width, height)) # need to add a way of removing the unwanted values for the following test
-    if val == 0: # Test for the second drawing of obj
-        # (16, 16, 16):
-
-    else: # Test for the third drawing of obj
-        # (32, 32, 32): """
-
+from PIL import Image, ImageDraw, ImageFilter
 
 def drawFirst(draw):
     # Randomly generate the sizes
     n = random.randint(6, 12)
     i = 1
     while i <= n:
-        x0 = random.randint(100, 600) # Starting point in the X direction defining the bounding box
-        y0 = random.randint(100, 600) # Starting point in the Y direction defining the bounding box
+        x0 = random.randint(130, 580) # Starting point in the X direction defining the bounding box
+        y0 = random.randint(130, 580) # Starting point in the Y direction defining the bounding box
         startCord = (x0, y0)
-        x1 = x0 + random.randint(275, 375) # X length
-        y1 = y0 + random.randint(275, 375) # Y length
+        x1 = x0 + random.randint(255, 345) # X length
+        y1 = y0 + random.randint(255, 345) # Y length
         endCord = (x1, y1)
-        draw.ellipse((startCord, endCord), fill='rgb(16, 16, 16)')
+        draw.ellipse((startCord, endCord), fill='rgb(32, 32, 32)')
         i += 1
 
 def drawSecond(draw):
@@ -44,7 +30,7 @@ def drawSecond(draw):
         x1 = x0 + random.randint(200, 300) # X length
         y1 = y0 + random.randint(200, 300) # Y length
         endCord = (x1, y1)
-        draw.ellipse((startCord, endCord), fill='rgb(32, 32, 32)')
+        draw.ellipse((startCord, endCord), fill='rgb(64, 64, 64)')
         i += 1
 
 def drawThird(draw):
@@ -55,19 +41,28 @@ def drawThird(draw):
         x0 = random.randint(180, 520) # Starting point in the X direction defining the bounding box
         y0 = random.randint(180, 520) # Starting point in the Y direction defining the bounding box
         startCord = (x0, y0)
-        x1 = x0 + random.randint(175, 275) # X length
-        y1 = y0 + random.randint(175, 275) # Y length
+        x1 = x0 + random.randint(100, 200) # X length
+        y1 = y0 + random.randint(100, 200) # Y length
         endCord = (x1, y1)
-        draw.ellipse((startCord, endCord), fill='rgb(64, 64, 64)')
+        draw.ellipse((startCord, endCord), fill='rgb(128, 128, 128)')
         i += 1
 
 def main():
     img = Image.new('RGB', (1081, 1081)) # About 16 pixels per km of map
     draw = ImageDraw.Draw(img)
+    # Draw the levels of elevation
     drawFirst(draw)
     drawSecond(draw)
     drawThird(draw)
-    img.save('generated_map.png') # Every itteration overwrites the last map
+    img.save('generated_map.png') # Save the map
+    # Cut out sharp corners of the topology
+    postImg1 = Image.open('generated_map.png')
+    simg1 = postImg1.filter(ImageFilter.ModeFilter(size = 20))
+    simg1.save('generated_map_post.png')
+    # Smooth the topology
+    postImg2 = Image.open('generated_map_post.png')
+    simg2 = postImg2.filter(ImageFilter.GaussianBlur(radius = 15))
+    simg2.save('generated_map_post.png')
 
 if __name__ == "__main__":
     main()
